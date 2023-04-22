8b47f3686f0b3f7728dff86077171f8599ac2e4c
Ábris Simon <abris96@gmail.com>
2023-04-22 18:46:33 +0200

    add meta tag to not allow safari to zoom in

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      ></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
